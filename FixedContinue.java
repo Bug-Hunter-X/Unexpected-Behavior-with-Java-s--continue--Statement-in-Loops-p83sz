5805d9d238a0dbfc10cf372e73c99d9786814889
BugHunter-X <askzenai@gmail.com>
2025-02-20 07:34:05 +0100

    Add FixedContinue.java file

---
+++ FixedContinue.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Iteration: " + i);
+            if (i == 3) {
+                i++; // Increment i before continuing
+                continue; // Skip the rest of the loop body for i == 3
+            }
+            System.out.println("After continue (if i ==3, this line won't execute for i=3): " + i);
+            i++;
+        }
+    }
+}
